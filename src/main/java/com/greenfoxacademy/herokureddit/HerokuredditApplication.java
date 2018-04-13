b4023424ece30754104f29f4866f75b03df6e12a
unknown <d.dns0098g@gmail.com>
2018-04-13 10:00:58 +0200

    init herokureddit

---
+++ src/main/java/com/greenfoxacademy/herokureddit/HerokuredditApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.herokureddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuredditApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HerokuredditApplication.class, args);
+    }
+}
